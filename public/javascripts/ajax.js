bd43dd3731990c33de4faeb173e3f8ecacf5573f
TECNOSYLVA\fgarcia <fernando.garcia@xeridia.com>
2016-10-19 15:24:19 +0200

    Improved API examples.

---
+++ public/javascripts/ajax.js
@@ -0,0 +1,17 @@
+$.each( [ "put", "delete" ], function( i, method ) {
+  $[ method ] = function( url, data, callback, type ) {
+    if ( $.isFunction( data ) ) {
+      type = type || callback;
+      callback = data;
+      data = undefined;
+    }
+
+    return $.ajax({
+      url: url,
+      type: method,
+      dataType: type,
+      data: data,
+      success: callback
+    });
+  };
+});
