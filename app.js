f495cecb775905973147837ae7ec4dad30da50d5
TECNOSYLVA\fgarcia <fernando.garcia@xeridia.com>
2016-10-20 15:09:15 +0200

    Added bootstrap-select selectpicker.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var models = require("./models");
+var i18n = require('vue-i18n');
 
 var routes = require('./routes/index');
 var users = require('./routes/api/users');
